29ceb9d2f5e91327a0d71e80550ba1dfbfb59ac1
Alina Paiu <apaiu@endava.com>
2016-10-03 15:28:21 +0300

    Hello World

--- src/test.java
+++ src/test.java
@@ -1,5 +1,11 @@
 /**
  * Created by apaiu on 10/3/2016.
  */
-public class test {
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
